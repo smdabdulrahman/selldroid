9a726687b1a620094b1278c938a62397ccde073e
Mohamed Abdul Rahman <abdul3rahman7@gmail.com>
2026-01-19 12:37:19 +0530

    changed com.example to com.buyp

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.selldroid"
+    namespace = "com.buyp.selldroid20"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.selldroid"
+        applicationId = "com.buyp.selldroid20"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/buyp/selldroid20/MainActivity.kt
+++ android/app/src/main/kotlin/com/buyp/selldroid20/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.selldroid
+package com.buyp.selldroid20
 
 import io.flutter.embedding.android.FlutterActivity
 
